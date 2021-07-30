4b30eabbed6d185a2996d64d11b220944024720a
Jörg Thalheim <joerg@thalheim.io>
2021-07-30 09:04:37 +0000

    fix evaluation on aarch64

--- default.nix
+++ default.nix
@@ -28,6 +28,7 @@
     # However one can use the upstream version
     x264
   ]);
+  multiPkgs = null;
   # for ocean_ncp this seems to be not defined correctly
   profile = pkgs.lib.optionalString (pkgs.stdenv.is64bit) ''
     export NIX_CFLAGS_COMPILE="$NIX_CFLAGS_COMPILE -DPTRDIFF_MAX=0x7fffffffffffffff"
